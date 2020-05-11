6812289df80a6e02bdb883a57d868483c4c7e449
Tobias Möritz <tobias@moeritz.cc>
2020-05-11 09:31:56 +0200

    🐛 fix twitter nr. 2

--- src/data/links.js
+++ src/data/links.js
@@ -1,7 +1,7 @@
 export default {
   www: "https://matthias.works",
   email: "mailto:hi@matthias.works",
-  twitter: "https://twitter.com/teaxofficial",
+  twitter: "https://twitter.com/matthiashalfmnn",
   dribbble: "https://dribbble.com/matthiaaas",
   github: "https://github.com/matthiaaas",
   behance: "https://behance.net/matthiaaas",
